6e95c0453686d974397b19ed70e8842596133e01
laura280 <lauramontoyac.01@gmail.com>
2020-08-29 17:34:01 -0500

    Ciclo for (clase video).

---
+++ Clases/sexta_clase.py
@@ -0,0 +1,10 @@
+#-----cilofor-----#
+for repeticion in range (2,10):
+    print(repeticion)
+print("fin, gracias")
+
+PreguntaNumero = "Hola, por favor ingrese un número entero para saber hasta donde debo contar : "
+numero = int (input(PreguntaNumero))
+
+for i in range (numero) :
+    print (i+1) 
