06b59da2ca56c41e883bc9c9713a36f1a8ce8e16
laura280 <lauramontoyac.01@gmail.com>
2020-09-08 13:48:36 -0500

    clase 8 finalzada

--- Clases/octava_clase.py
+++ Clases/octava_clase.py
@@ -4,11 +4,22 @@ dado = random.randint (1,6)
 print(dado)
 
 NumeroParticipante = int(input("Por favor ingrese un número: "))
+intentos = 1
+
 while (NumeroParticipante != dado): 
     print("No adivinó el número")
+    intentos =intentos + 1
     NumeroParticipante = int(input("Por favor ingrese un número: "))
 
-print("Adivinó el número")
+print(f"Ha realizado {intentos} intentos antes de adivinar el número")
+print("felicitaciones, adivinó el número")
 
 listaalimentos = []
-decision =
+listaalimentos = ["pastas, papas fritas, carne, helado, empanadas,"]
+sizelist =len (listaalimentos)
+print (sizelist)
+
+for i in range (sizelist):
+    print(f"las {listaalimentos [i]} son de mis comidas favoritas")
+
+print(listaalimentos[2])
