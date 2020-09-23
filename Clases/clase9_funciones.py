835d35835c00bb029ec5d82e7f1d6307a75408e9
laura280 <lauramontoyac.01@gmail.com>
2020-09-22 21:49:13 -0500

    clase video 1, no completa

---
+++ Clases/clase9_funciones.py
@@ -0,0 +1,21 @@
+#Funciones sin entrada
+print ("#"*60)
+print ("hola")
+print ("¿Cómo está?")
+print ("#"*60)
+def lineDesign():
+    print("#"*60)
+lineDesign()
+print ("hola")
+print ("¿Cómo está?")
+lineDesign()
+
+#Funciones con una entrada
+def lineDesignC(cantidad=2):
+    print("#"*cantidad)
+lineDesignC(12)
+lineDesignC()
+
+#Función suma
+def sumar (valor1, valor2):
+    return valor1+
