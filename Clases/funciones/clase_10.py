c350cde6bd3e39ec32108c9734e3c6b646690d57
laura280 <lauramontoyac.01@gmail.com>
2020-10-02 14:36:29 -0500

    Examen 3

--- Clases/funciones/clase_10.py
+++ Clases/funciones/clase_10.py
@@ -1,3 +1,4 @@
+#FUNCIONES#
 #Devolver numero mayor o decir si son iguales
 a = int(input("Pro favor ingrese el primer número entero :")) 
 b = int(input("Por favor ingrese el segundo número entero :")) 
@@ -9,7 +10,47 @@ def infNumero (numero1, numero2):
     elif (a>b):
         print ("El primer número ingresado", a, "es mayor que e segundo número ingresado", b)
     else :
-        print (f"el número B {b} es mayor que A {a}")
+        print (f"el número  {b} es mayor que {a}")
 
 infNumero (a,b)
 infNumero (7,8)
+
+
+#En una lista, mostrar nombre por nombre
+def mostrarLista (lista):
+    for elemento in lista :
+        print (elemento)
+
+listaNombres = ["Laura", "Karla", "Juan", "Daniel"]
+mostrarLista (listaNombres)
+
+
+#Devolver el IMC
+def calcularIMC (peso, altura):
+    return peso / (altura**2)
+imc = calcularIMC (56,1.52)
+print (imc)
+
+
+#Dada la función imc y los valores de peso y altura mostrar el IMC
+def mostrarIMC (funcionIMC, estatura, peso):
+    imc = funcionIMC (estatura,peso)
+    print (f"el IMC calculado es {imc}")
+
+mostrarIMC(calcularIMC, 1.52,56)
+
+
+#CLASES Y OBJETOS#
+#Objeto con id, nombre, peso, altura, sexo
+
+class Persona:
+    def __init__ (self, idIngresado, nombreIngresado, pesoIngresado, alturaIngresada, sexoIngresado):
+        self.id = idIngresado
+        self.nombre = nombreIngresado
+        self.peso = pesoIngresado
+        self.estatura = alturaIngresada
+        self.sexo = sexoIngresado
+        print ("Se ha creado un nuevo objeto persona")
+    def atributos (self):
+        print (f"Hola mi nombre es {self.nombre}, soy de sexo {self.sexo},")
+
---
+++ Examenes/Examen3/examen3.py
@@ -0,0 +1,92 @@
+#Función por cada uno de los puntos#
+
+##devolver número elevado al cuadrado
+def numeroalCuadrado (numero):
+    return numero**2
+num1= numeroalCuadrado(6)
+print (f"El número dado elevado a la dos es {num1}") 
+
+##devolver número elevado a la 3
+def numeroalCubo (numero):
+    return numero**3
+num2= numeroalCubo(6)
+print (f"El número dado elevado a la tres es {num2}") 
+
+##devolver número elevado a la 4
+def numeroalaCuatro (numero):
+    return numero**4
+num3= numeroalaCuatro(6)
+print(f"El número dado elevado a la cuatro es {num3}") 
+
+##devolver número elevado a la 5
+def numeroalaCinco (numero):
+    return numero**5
+num4= numeroalaCinco(6)
+print(f"El número dado elevado a la cinco es {num4}") 
+
+##Función dada otra función anterior muestre el resultado de las mismas
+def operacionNueroElev (operacion, numero):
+    print (operacion(numero))
+operacionNueroElev (numeroalCuadrado,5)
+operacionNueroElev (numeroalCubo,4)
+operacionNueroElev (numeroalaCuatro,8)
+operacionNueroElev (numeroalaCinco,9)
+
+#Clases#
+
+#Doctor
+
+class Doctor:
+    def __init__ (self, idIng, nombreIng, sexoIng, universidadIng) :
+        self.id = idIng
+        self.nombre = nombreIng
+        self.sexo = sexoIng
+        self.universidad = universidadIng
+    def atributos(self):
+        print(f"Hola soy el Doctor {self.nombre} de sexo {self.sexo} y soy egresado de la universidad {self.universidad}")
+    def sintomas (self, lista):
+        for elemento in  lista:
+            print (elemento)
+
+doctor1 = Doctor (21365289, "Felipe", "masculino", "Ces")
+listaSintomas = ["tos", "dolor de cabeza", "fiebre", "malestar", "dolor de garganta"]
+doctor1.atributos()
+doctor1.sintomas(listaSintomas)
+
+#Neurologo
+class Neurologo (Doctor):
+    def __init__ (self, idIng, nombreIng, sexoIng, universidadIng, experienciaIng, consultorioIng, salarioIng) :
+        Doctor.__init__(self, idIng, nombreIng, sexoIng, universidadIng)
+        self.experiencia = experienciaIng
+        self.consultorio = consultorioIng
+        self.salario = salarioIng
+    def atributos (self):
+        print(f"Hola soy el Doctor {self.nombre} de sexo {self.sexo} y soy egresado de la universidad {self.universidad} con experiencia de {self.experiencia}, trabajo en el consultorio {self.consultorio} y tengo un salario de {self.salario}")
+    def pacientes (self, lista):
+        for elemento in lista:
+            print (f"el paciente a seguir es : {elemento}")
+
+doctor1= Neurologo (682746834, "Felipe", "masculino", "ces", 5, 107, 7200000 )
+listaPacientes = ["Camilo", "Esteban", "Julian", "Mariana"]
+doctor1.atributos()
+doctor1.pacientes(listaPacientes)
+
+#Cárdiologo
+class Cardiologo (Doctor):
+    def __init__ (self, idIng, nombreIng, sexoIng, universidadIng, experienciaIng, consultorioIng, salarioIng, habitacionesIng) :
+        Doctor.__init__(self, idIng, nombreIng, sexoIng, universidadIng)
+        self.habitaciones = habitacionesIng
+        self.experiencia= experienciaIng
+        self.salario = salarioIng
+        self.consulturio = consultorioIng
+    def atributos (self):
+        print (f"Hola soy el Doctor {self.nombre} de sexo {self.sexo} y soy egresado de la universidad {self.universidad} con experiencia de {self.experiencia}, trabajo en el consultorio {self.consultorio} y tengo un salario de {self.salario}, hay {self.habitaciones} disponibles")
+    def sintomas (self, lista):
+        for elemento in  lista:
+            print (elemento)
+            print ("Ya sé que tiene el paciente")
+
+doctor3=Cardiologo (654348326, "Pedro", "Masculino", "UPB", 7, 23, 8000000, 7)
+listaSintomas2 = ["tos", "dolor de cabeza", "fiebre", "malestar", "dolor de garganta"]
+doctor3.atributos()
+doctor3.sintomas(listaSintomas2)
